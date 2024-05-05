95a9b98c57626cdef9e3e57752dbf140177c855e
Fredhardycnm <167549226+Fredhardycnm@users.noreply.github.com>
2024-05-05 19:24:28 +0800

    Add files via upload

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2024-2026 夕颜韵白</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
